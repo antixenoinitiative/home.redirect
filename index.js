dd131fcba6345327ea4232c0203748a675cb6d8a
CMDRMgram <marcus@ingrams.cloud>
2021-09-13 16:25:12 +1000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 // create a http server
 http.createServer(function (req, res) {
     if (req.url == '/wiki') {
-        res.writeHead(301, { "Location": "http://wiki.antixenoinitiative.com" });
+        res.writeHead(301, { "Location": "http://wiki.antixenoinitiative.com" + req.url });
         return res.end();
     } else {
         res.writeHead(302, { "Location": "http://www.antixenoinitiative.com" + req.url });
