0ca255a81f4bf9694f0b46fafe5e646e0eb13be3
CMDRMgram <marcus@ingrams.cloud>
2021-09-13 16:10:08 +1000

    Setup

---
+++ index.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var fs = require('fs');
+ 
+// create a http server
+http.createServer(function (req, res) {
+    if (req.url == '/wiki') {
+        res.writeHead(301, { "Location": "http://wiki.antixenoinitiative.com" });
+        return res.end();
+    } else {
+        res.writeHead(302, { "Location": "http://www.antixenoinitiative.com" + req.url });
+        return res.end();
+    }
+}).listen(8085);
